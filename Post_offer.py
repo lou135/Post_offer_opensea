a1d17ad756c04011844acb9e1ba64eeeab6d461a
lou135 <122233597+lou135@users.noreply.github.com>
2024-07-11 13:22:03 +0100

    Add files via upload

--- Post_offer.py
+++ Post_offer.py
@@ -3,40 +3,42 @@ from random import randint
 from eth_account import Account
 from eth_account.messages import *
 import time
+from creds import private_key, api_os
 
-private_key = "MY_PRIVATE_KEY"
+private_key = private_key
+api_os=api_os
 url = "https://api.opensea.io/api/v2/orders/matic/seaport/offers"
 startTime= int(time.time())
-endTime= startTime + 43200
+endTime= int(startTime + 43200)
 salt= randint(1,100000)
 parameters = {
     "orderType": 3,
-    "offerer": "0x6392509eF0306B1Fc6AED0B5210e7750450aAc07",
+    "offerer": "0x900d18C41C1a13A757d66EC0Cd02c32c8446f509",
     "offer": [
         {
             "itemType": 1,
             "token": "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
             "identifierOrCriteria": 0,
-            "startAmount": 20000,
-            "endAmount": 20000
+            "startAmount": 15000,
+            "endAmount": 15000
         }
     ],
     "consideration": [
         {
             "itemType": 3,
             "token": "0x22d5f9B75c524Fec1D6619787e582644CD4D7422",
-            "identifierOrCriteria": 909,
+            "identifierOrCriteria": 457,
             "startAmount": 1,
             "endAmount": 1,
-            "recipient": "0x6392509eF0306B1Fc6AED0B5210e7750450aAc07"
+            "recipient": "0x900d18C41C1a13A757d66EC0Cd02c32c8446f509"
         },
         {
             "itemType": 1,
             "token": "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
             "identifierOrCriteria": 0,
-            "startAmount": 500,
-            "endAmount": 500,
-            "recipient": "0x0000a26b00c1F0DF003000390027140000FAa719"
+            "startAmount": 375,
+            "endAmount": 375,
+            "recipient": "0x0000a26b00c1F0DF003000390027140000FAa719" 
         }
     ],
     "startTime": startTime,
@@ -100,15 +102,31 @@ encoded_msg = encode_typed_data(full_message=msg)
 signed_message = Account.sign_message(encoded_msg, private_key=private_key)
 signature = signed_message.signature.hex()
 
+parameters["startTime"]=str(parameters["startTime"])
+parameters["endTime"]=str(parameters["endTime"])
+parameters["salt"]=str(parameters["salt"])
+parameters["totalOriginalConsiderationItems"]=str(parameters["totalOriginalConsiderationItems"])
+
+parameters["offer"][0]["identifierOrCriteria"]=str(parameters["offer"][0]["identifierOrCriteria"])
+parameters["offer"][0]["startAmount"]=str(parameters["offer"][0]["startAmount"])
+parameters["offer"][0]["endAmount"]=str(parameters["offer"][0]["endAmount"])
+
+parameters["consideration"][0]["identifierOrCriteria"]=str(parameters["consideration"][0]["identifierOrCriteria"])
+parameters["consideration"][0]["startAmount"]=str(parameters["consideration"][0]["startAmount"])
+parameters["consideration"][0]["endAmount"]=str(parameters["consideration"][0]["endAmount"])
+parameters["consideration"][1]["identifierOrCriteria"]=str(parameters["consideration"][1]["identifierOrCriteria"])
+parameters["consideration"][1]["startAmount"]=str(parameters["consideration"][1]["startAmount"])
+parameters["consideration"][1]["endAmount"]=str(parameters["consideration"][1]["endAmount"])
+
 payload = {
     "protocol_address": "0x0000000000000068f116a894984e2db1123eb395",
     "parameters": parameters,
-    "signature": signature
+    "signature": str(signature)
 }
 headers = {
     "accept": "application/json",
     "content-type": "application/json",
-    "x-api-key": "MY_API_KEY"
+    "x-api-key": f"{api_os}"
 }
 
 response = requests.post(url, json=payload, headers=headers)
